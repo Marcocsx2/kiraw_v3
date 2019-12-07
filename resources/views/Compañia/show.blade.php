ba7e7dba2ba6991b0675852c9cb439844f2b1b5a
Marcocsx <marco.vizcarra@tecsup.edu.pe>
2019-12-06 20:59:25 -0500

    compañias
    compias

--- resources/views/Compañia/show.blade.php
+++ resources/views/Compañia/show.blade.php
@@ -28,8 +28,9 @@
 }
 
 </style>
+@foreach ($compañias as $compañia)
 <div class="container-fluid">
-<div class="profile-header-background  z-depth-2"><img class="" src="{{$perfiles->fondo}}" alt="Profile Header Background"></div>
+<div class="profile-header-background  z-depth-2"><img class="" src="{{$compañia->pro_imagen}} " alt="Profile Header Background"></div>
     <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
         <div class="user-profile">
@@ -39,8 +40,8 @@
                 <div class="col-md-4">
                     <div class="profile-info-left">
                         <div class="text-center">
-                        <img src="{{$perfiles->imagen}}" alt="Avatar" class="avatar rounded-circle img-circle">
-                            <h2>{{$perfiles->name}}</h2>
+                        <img src="{{$compañia->pro_imagen}}" alt="Avatar" class="avatar rounded-circle img-circle">
+                            <h2>{{$compañia->pro_nombre}}</h2>
                         </div>
                     </div>
                 </div>
@@ -57,7 +58,8 @@
         </div>
     </div>
 </div>
-</div>    
+</div>
+@endforeach   
     
     <br>
     <br>
